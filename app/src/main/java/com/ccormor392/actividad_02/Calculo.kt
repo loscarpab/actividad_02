3e5955ead37b319e0f6ca13733665baf782036cc
loscarpab <ccormor392@g.educaand.es>
2023-10-10 21:30:13 +0200

    clase Calculo creada

---
+++ app/src/main/java/com/ccormor392/actividad_02/Calculo.kt
@@ -0,0 +1,21 @@
+package com.ccormor392.actividad_02
+
+class Calculo {
+    var num1 = 0
+    var num2 = 0
+    var operacion = ""
+    var resultado = 0
+    fun sumar(): Int {
+        return num1 + num2
+    }
+    fun multiplicar(): Int {
+        return num1 * num2
+    }
+    fun dividir(): Int {
+        return num1 / num2
+    }
+    fun restar(): Int {
+        return num1 - num2
+    }
+
+}
--- app/src/main/java/com/ccormor392/actividad_02/MainActivity.kt
+++ app/src/main/java/com/ccormor392/actividad_02/MainActivity.kt
@@ -2,10 +2,58 @@ package com.ccormor392.actividad_02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    var calc = Calculo()
+    lateinit var numeros:MutableList<Button>
+    lateinit var operaciones:MutableList<Button>
+    lateinit var botonCE: Button
+    lateinit var texto: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initVariables()
+        initListeners()
+
+    }
+    fun initVariables(){
+        numeros = crearListaNumeros()
+        operaciones = crearListaOperaciones()
+        botonCE = findViewById(R.id.ce)
+        texto = findViewById(R.id.textNumbers)
+    }
+
+    fun crearListaNumeros():MutableList<Button>{
+        val listButtons = mutableListOf<Button>()
+        listButtons.add(findViewById(R.id.one))
+        listButtons.add(findViewById(R.id.two))
+        listButtons.add(findViewById(R.id.three))
+        listButtons.add(findViewById(R.id.four))
+        listButtons.add(findViewById(R.id.five))
+        listButtons.add(findViewById(R.id.six))
+        listButtons.add(findViewById(R.id.seven))
+        listButtons.add(findViewById(R.id.eight))
+        listButtons.add(findViewById(R.id.nine))
+        return listButtons
+    }
+    fun crearListaOperaciones():MutableList<Button>{
+        val listButtons = mutableListOf<Button>()
+        listButtons.add(findViewById(R.id.plus))
+        listButtons.add(findViewById(R.id.minus))
+        listButtons.add(findViewById(R.id.product))
+        listButtons.add(findViewById(R.id.divide))
+        return listButtons
+    }
+    fun initListeners(){
+        for (boton in numeros){
+            boton.setOnClickListener { listenerNumeros(boton) }
+        }
+    }
+    fun listenerNumeros(boton:Button){
+        texto.text = boton.text
     }
 }
